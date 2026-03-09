8b9c327e29555fa07a5dc1ad4d791ebefa3bcf2c
ManjuV <software4manju@gmail.com>
2026-03-09 15:04:19 -0400

    Sign in to GitHub

--- pages/SignInToGitHubPage.ts
+++ pages/SignInToGitHubPage.ts
@@ -4,32 +4,33 @@ import { BasePage } from "./BasePage";
 export class SignInToGitHubPage extends BasePage {
 
     readonly usernameId = "login_field";
-    readonly passwordID = "password";
-    readonly signInButtonID = "commit";
+    readonly passwordID = "password";  
     
     constructor(page: Page) {
         super(page);        
    
 }
-    async enterwrongUsername(values: string) {     
+    async enterwrongUsername(values: string) {
         await this.page.locator(this.usernameId).fill(values);
     }
-     async enterUsername(values: string) {     
-        await this.page.locator(`#${this.usernameId}`).fill(values);
-    }
+    async enterUsername(values: string) {
+    console.log("Username: " + values);
+
+    await this.page.waitForSelector(`#${this.usernameId}`, { state: 'visible' });
+
+    await this.page.locator(`#${this.usernameId}`).fill(values);
+}
     async enterPassword(values: string) {     
-        await this.page.locator(this.passwordID).fill(values);
+        await this.page.locator(`#${this.passwordID}`).fill(values);
     }   
 
     async clickSignInButton() {
-        await this.page.locator(this.signInButtonID).click();
+        await this.page.getByRole('button', { name: 'Sign in' }).click();       
     }
 
     async clickGitHubLink() {
        await this.page.getByRole("link", { name: "GitHub repository" }).click();
        await this.page.setViewportSize({ width: 1280, height: 800 });
-       const signinLink = await this.page.getByRole("link", { name: "Sign in" });
-       await signinLink.click();
     }
 
 }
--- tests/createRepoInGitHub.spec.ts
+++ tests/createRepoInGitHub.spec.ts
@@ -3,13 +3,29 @@ import { HomePage } from '../pages/HomePage';
 import { SignInToGitHubPage } from '../pages/SignInToGitHubPage';
 
 test('Sign In To GitHub', async ({ page }) => {
+
   await page.goto('https://playwright.dev/');
+  const homePage = new HomePage(page);
+
+   //Learn: Promise.all() runs multiple async actions at the same time and waits until all of them finish.
+  //Learn:waitForEven(page) returns a new object githubPage.
+
   const signInToGitHubPage = new SignInToGitHubPage(page);
 
-  await signInToGitHubPage.clickGitHubLink();
-  await signInToGitHubPage.enterUsername("software4manju");
-  await signInToGitHubPage.enterPassword("Manju#123");
-  
-  await signInToGitHubPage.clickSignInButton();
-});
+  // Opens to a new tab when we click on GitHub link, need to handle that
+   const [githubPage] =   await Promise.all([
+    page.context().waitForEvent('page'),
+     signInToGitHubPage.clickGitHubLink()]);
+     
+   await githubPage.waitForLoadState();  
 
+   const gitHubPagesignInTo = new SignInToGitHubPage(githubPage);
+
+
+  await githubPage.getByRole('link', { name: 'Sign in' }).click();
+  await gitHubPagesignInTo.enterUsername("software4manju");
+  await gitHubPagesignInTo.enterPassword("Manju#123");
+
+  await gitHubPagesignInTo.clickSignInButton();
+  console.log("Successfully signed in to GitHub");
+});
